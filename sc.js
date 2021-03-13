017559a2c9c78f7a8cecd8d9451542f6a3ab34e4
Kaizenfxm <80600956+Kaizenfxm@users.noreply.github.com>
2021-03-13 16:52:13 -0500

    Create sc.js

---
+++ sc.js
@@ -0,0 +1,21 @@
+function totalCo( obj ) {
+  var sum = 0;
+  for( var el in obj ) {
+    if( obj.hasOwnProperty( el ) ) {
+      sum += parseFloat( obj[el] );
+    }
+  }
+  return sum;
+}
+
+
+function countCart(obj) {
+    var count = 0;
+
+    for(var prop in obj) {
+        if(obj.hasOwnProperty(prop))
+            ++count;
+    }
+
+    return count;
+}
